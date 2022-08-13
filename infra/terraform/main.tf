bf7e3b0c66efb167b424d62345b4e1f3275d8d70
Zhu Zhanyan <program.nom@gmail.com>
2022-08-13 15:01:29 +0800

    feat(terraform): Deploy GKE Cluster & GCS Buckets (#6)
    * chore: add precommmit hooks to run linters for python & terraform)
    
    * fix: spelling detected by codespell
    
    * fixup! chore: add precommmit hooks to run linters for python & terraform)
    
    * feat(infra): deploy gcs bucket to store terraform state
    
    * chore(git): git ignore terraform files
    
    * build(infra): add terraform provider plugin lock file
    
    * feat(terraform): add gcs buckets to store raw & processed training datasets
    
    * ci(terraform): add github actions workflow to lint, plan & apply terraform
    
    * ci(terraform): fix typo in plan-terraform job name in needs dependency
    
    * chore: disable color codes for terraform validate pre commit hook output
    
    * feat(terraform): deploy autopilot GKE k8s cluster with private worker nodes
    
    * ci: merge lint & apply jobs into the same job & expose gcp key to all steps of job
    
    * refactor(terraform): extract gcp region into local.region local var
    
    * fix(terraform): add ip_allocation_policy hotfix
    
    https://github.com/hashicorp/terraform-provider-google/issues/10782
    
    * Update .pre-commit-config.yaml

---
+++ infra/terraform/main.tf
@@ -0,0 +1,65 @@
+#
+# sortin-hat
+# Terraform Infrastructure
+#
+
+locals {
+  project_id = "sss-sortin-hat"
+  region     = "asia-southeast1" # Google's SG, Jurong West datacenter
+}
+terraform {
+  required_version = "~>1.2.6"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.31.0"
+    }
+  }
+  backend "gcs" {
+    bucket = "sss-sortin-hat-terraform-state"
+    prefix = "terraform/state"
+  }
+}
+
+provider "google" {
+  project = local.project_id
+  region  = local.region
+}
+
+# GCS buckets
+# store terraform state
+resource "google_storage_bucket" "tf_state" {
+  name     = "${local.project_id}-terraform-state"
+  location = local.region
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+# raw data source files
+resource "google_storage_bucket" "raw" {
+  name     = "${local.project_id}-raw"
+  location = local.region
+}
+# processed datasets for training ML models
+resource "google_storage_bucket" "datasets" {
+  name     = "${local.project_id}-datasets"
+  location = local.region
+}
+
+# GKE K8s Cluster
+resource "google_container_cluster" "main" {
+  name             = "main"
+  enable_autopilot = true
+  location         = local.region
+
+  # hotfix: https://github.com/hashicorp/terraform-provider-google/issues/10782
+  ip_allocation_policy {
+  }
+
+  private_cluster_config {
+    # disable public internet access to worker nodes
+    enable_private_nodes    = true
+    enable_private_endpoint = false # allow public access to k8s endpoint
+  }
+}
