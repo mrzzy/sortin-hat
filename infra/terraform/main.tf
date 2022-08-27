e5b69241d6c2f948c208c697e465a450414d20bb
Zhu Zhanyan <program.nom@gmail.com>
2022-08-27 16:16:49 +0800

    feat(terraform): create service account for ml pipline containers to access GCS buckets

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -18,6 +18,8 @@ locals {
     "models"
   ] : "${local.project_id}-${suffix}"])
 }
+
+# Terraform & Provider config
 terraform {
   required_version = "~>1.2.6"
   required_providers {
@@ -25,16 +27,26 @@ terraform {
       source  = "hashicorp/google"
       version = "4.31.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.13.0"
+    }
   }
   backend "gcs" {
     bucket = "sss-sortin-hat-terraform-state"
     prefix = "terraform/state"
   }
 }
-
 provider "google" {
   project = local.project_id
   region  = local.region
+}
+provider "kubernetes" {
+  host = "https://${google_container_cluster.main.endpoint}"
+
+  client_certificate     = base64decode(google_container_cluster.main.master_auth.0.client_certificate)
+  client_key             = base64decode(google_container_cluster.main.master_auth.0.client_key)
+  cluster_ca_certificate = base64decode(google_container_cluster.main.master_auth.0.cluster_ca_certificate)
 }
 
 # GCS buckets
@@ -48,11 +60,48 @@ resource "google_storage_bucket" "tf_state" {
   }
 }
 
-
+# Google Cloud Storage
 resource "google_storage_bucket" "buckets" {
   for_each = local.buckets
   name     = each.key
   location = local.region
+}
+
+# GCP IAM
+# service account to allow ML pipeline containers to access GCS Buckets
+resource "google_service_account" "pipeline" {
+  account_id   = "ml-pipeline"
+  display_name = "Service account used by ML pipeline containers to access worker nodes"
+}
+resource "google_storage_bucket_iam_member" "allow_k8s" {
+  for_each = local.buckets
+  bucket   = each.key
+  role     = "roles/storage.objectAdmin" # allow CRUD operations on object
+  member   = "serviceAccount:${google_service_account.pipeline.email}"
+}
+# ml pipeline service account as k8s secret
+resource "google_service_account_key" "pipeline" {
+  service_account_id = google_service_account.pipeline.id
+}
+resource "kubernetes_secret" "pipeline_svc_acc_key" {
+  metadata {
+    name = "ml-pipeline-gcp-service-account"
+    labels = {
+      "app.kubernetes.io/part-of"    = "sortin-hat"
+      "app.kubernetes.io/component"  = "ml-pipeline"
+      "app.kubernetes.io/managed-by" = "terraform"
+    }
+
+  }
+  data = {
+    "private-key" = google_service_account_key.pipeline.private_key
+  }
+}
+
+# service account for GKE worker nodes
+resource "google_service_account" "k8s_node" {
+  account_id   = "k8s-cluster-node"
+  display_name = "Service account used by GKE 'main' K8s cluster worker nodes"
 }
 
 # GKE K8s Cluster
@@ -67,10 +116,6 @@ resource "google_container_cluster" "main" {
   initial_node_count       = 1
 }
 # node pool for running long-running support infrastructure (ie. Airflow, MLFlow)
-resource "google_service_account" "k8s_node" {
-  account_id   = "k8s-cluster-node"
-  display_name = "Service account used by GKE 'main' K8s cluster worker nodes"
-}
 resource "google_container_node_pool" "infra" {
   name       = "support-infra"
   cluster    = google_container_cluster.main.id
