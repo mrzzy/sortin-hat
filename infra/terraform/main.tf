feafbdea00fb014f31a5617cd4a6d4b611a54f38
Zhu Zhanyan <program.nom@gmail.com>
2022-08-27 18:09:35 +0800

    feat(k8s): Deploy MLFlow on GKE for Experiment Tracking (#8)
    * chore(git): ignore .*.env secret dotenv files
    
    * refactor(k8s): pass postgres credentials from postgres.env dotenv
    
    * refactor(k8s): airflow to use externally deployed postgresql
    
    * fix(k8s): typo in secret key used to 'postgres' user password to postgresql db
    
    * refactor(k8s): deploy helm charts with 'ml-pipeline' release name
    
    to make components of ML Pipeline easier to identify
    
    * feat(k8s): add initdb script to CREATE DATABASE for airflow deployment
    
    * feat(k8s): deploy mlflow using helm chart
    
    * refactor(terraform): rename raw data gcs bucket to clarify naming
    
    renamed 'sss-sortin-hat-raw' to 'sss-sortin-hat-raw-data'
    
    * feat(k8s): config mlflow to use sss-sortin-hat-models gcs bucket to store artifacts
    
    * refactor(terraform): deuplicate bucket resources with for_each meta arg
    
    * feat(terraform): create service account for ml pipline containers to access GCS buckets
    
    * fix(terraform): terraform kubernetes provider auth with GKE
    
    * fix(terraform): remove ml-pipeline svc acc key base64 encoding before applying as k8s secret
    
    * feat(k8s): patch mlflow deployment with use ml-pipeline service account key
    
    * refactor(k8s): suffix 'mlflow' to mlflow resources clarify naming

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -7,7 +7,19 @@ locals {
   project_id = "sss-sortin-hat"
   # Google's SG, Jurong West datacenter
   region = "asia-southeast1"
+
+  # name of the GCS buckets to create
+  buckets = toset([for suffix in [
+    # raw, unprocessed source data files
+    "raw-data",
+    # processed datasets for training ML models
+    "datasets",
+    # trained ML model artifacts
+    "models"
+  ] : "${local.project_id}-${suffix}"])
 }
+
+# Terraform & Provider config
 terraform {
   required_version = "~>1.2.6"
   required_providers {
@@ -15,16 +27,28 @@ terraform {
       source  = "hashicorp/google"
       version = "4.31.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.13.0"
+    }
   }
   backend "gcs" {
     bucket = "sss-sortin-hat-terraform-state"
     prefix = "terraform/state"
   }
 }
-
 provider "google" {
   project = local.project_id
   region  = local.region
+}
+
+# fetch access token using Google Application Default credentials to authenticate
+# terraform's kubernetes provider when applying resources.
+data "google_client_config" "default" {}
+provider "kubernetes" {
+  host                   = "https://${google_container_cluster.main.endpoint}"
+  cluster_ca_certificate = base64decode(google_container_cluster.main.master_auth.0.cluster_ca_certificate)
+  token                  = data.google_client_config.default.access_token
 }
 
 # GCS buckets
@@ -37,26 +61,55 @@ resource "google_storage_bucket" "tf_state" {
     prevent_destroy = true
   }
 }
-# raw data source files
-resource "google_storage_bucket" "raw" {
-  name     = "${local.project_id}-raw"
+
+# Google Cloud Storage
+resource "google_storage_bucket" "buckets" {
+  for_each = local.buckets
+  name     = each.key
   location = local.region
 }
-# processed datasets for training ML models
-resource "google_storage_bucket" "datasets" {
-  name     = "${local.project_id}-datasets"
-  location = local.region
+
+# GCP IAM
+# service account to allow ML pipeline containers to access GCS Buckets
+resource "google_service_account" "pipeline" {
+  account_id   = "ml-pipeline"
+  display_name = "Service account used by ML pipeline containers to access worker nodes"
 }
-# trained ML model artifacts
-resource "google_storage_bucket" "models" {
-  name     = "${local.project_id}-models"
-  location = local.region
+resource "google_storage_bucket_iam_member" "allow_k8s" {
+  for_each = local.buckets
+  bucket   = each.key
+  role     = "roles/storage.objectAdmin" # allow CRUD operations on object
+  member   = "serviceAccount:${google_service_account.pipeline.email}"
+}
+# ml pipeline service account as k8s secret
+resource "google_service_account_key" "pipeline" {
+  service_account_id = google_service_account.pipeline.id
+}
+resource "kubernetes_secret_v1" "pipeline_svc_acc_key" {
+  metadata {
+    name = "ml-pipeline-gcp-service-account"
+    labels = {
+      "app.kubernetes.io/part-of"    = "sortin-hat"
+      "app.kubernetes.io/component"  = "ml-pipeline"
+      "app.kubernetes.io/managed-by" = "terraform"
+    }
+
+  }
+  data = {
+    "private-key" = base64decode(google_service_account_key.pipeline.private_key)
+  }
+}
+
+# service account for GKE worker nodes
+resource "google_service_account" "k8s_node" {
+  account_id   = "k8s-cluster-node"
+  display_name = "Service account used by GKE 'main' K8s cluster worker nodes"
 }
 
 # GKE K8s Cluster
 resource "google_container_cluster" "main" {
   name = "main"
-  # deploy zonal cluster as regional cluster will spin up 1 node per zone (3)
+  # deploy a zonal cluster as regional cluster will spin up 1 node per zone (3)
   # which is too much for our requirements
   location = "${local.region}-c"
 
@@ -65,10 +118,6 @@ resource "google_container_cluster" "main" {
   initial_node_count       = 1
 }
 # node pool for running long-running support infrastructure (ie. Airflow, MLFlow)
-resource "google_service_account" "k8s_node" {
-  account_id   = "k8s-cluster-node"
-  display_name = "Service account used by GKE 'main' K8s cluster worker nodes"
-}
 resource "google_container_node_pool" "infra" {
   name       = "support-infra"
   cluster    = google_container_cluster.main.id
