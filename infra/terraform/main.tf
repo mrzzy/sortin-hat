de707d15868bf99af859e9d6b4c4f2dc67c7b7fa
Zhu Zhanyan <program.nom@gmail.com>
2022-09-03 10:04:28 +0800

    feat(k8s): Deploy Ray Cluster for Distributed Training & Hyperparameter tuning (#9)
    * feat(k8s): deploy kuberay - operator for ray clusters
    
    significantly slows down kustomize runtime as it deploys from a github
    hosted kustomization.
    
    * perf(k8s): scale up to e2-standard-2 gke worker node machine type due to memory pressure
    
    * feat(k8s): add ray cluster CRD to deploy ray cluster
    
    * perf(k8s): scale up worker node for ray to e2-highcpu-4 machine type and 30 GB storage
    
    Ray cluster had issues scheduling to disk pressure & insufficient CPU
    
    * perf(k8s): scale up to e2-standard-4 due to memory pressure

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -124,8 +124,8 @@ resource "google_container_node_pool" "infra" {
   node_count = 1
 
   node_config {
-    machine_type    = "e2-medium"
+    machine_type    = "e2-standard-4"
     service_account = google_service_account.k8s_node.email
-    disk_size_gb    = 15
+    disk_size_gb    = 30
   }
 }
