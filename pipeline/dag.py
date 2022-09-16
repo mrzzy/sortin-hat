62e4c11f56416a688456600ba206d03f8edfc541
Zhu Zhanyan <program.nom@gmail.com>
2022-09-16 04:53:35 +0000

    refactor(pipeline): allow path wher sec4 cohort & p6 excel files are stored to be configured

--- pipeline/dag.py
+++ pipeline/dag.py
@@ -15,8 +15,14 @@ from pendulum import date
 
 config = {
     "buckets": {
-        "raw_data": "sss-sortin-hat-raw-data",
-        "dataset": "sss-sortin-hat-datasets",
+        "raw_data": {
+            "name": "sss-sortin-hat-raw-data",
+            "sec4_prefix": "Sec4_Cohort",
+            "p6_prefix": "P6_Screening",
+        },
+        "dataset": {
+            "name": "sss-sortin-hat-datasets",
+        }
     }
 }
 
@@ -42,27 +48,28 @@ def pipeline():
 
     ## Input
     The Data & ML Pipeline expects the following spreadsheets as inputs:
-    - Sec 4 Cohort Sendout: stored under 'Sec4_Cohort/<YEAR>.xlsx'
-    - P6 Screening Template: stored under 'P6_Screening/<YEAR>.xlsx'
+    - Sec 4 Cohort Sendout: stored under `<SEC4_PREFIX>/<YEAR>.xlsx`
+    - P6 Screening Template: stored under `<P6_PREFIX>/<YEAR>.xlsx`
+    > `<SEC4_PREFIX>` & `<P6_PREFIX>` can are configured with
+    > the `bucket.raw_data.sec4_prefix` & `bucket.raw_data.p6_prefix`.
 
-    The Excel spreadsheet should be partitioned by year & stored in the 'raw_data'
-    GCS Bucket.
+    The Excel spreadsheet should be partitioned by year & stored in the 
+    `bucket.raw_data.name` GCS Bucket.
 
     ## Outputs
     MLFlow Models & Evaluation results from the ML training process stored in the 
-    'models' directory.
+    `bucket.models.name` GCS bucket.
 
     """
     @task(
         task_id="etl_dataset"
-        description
     )
-    def etl_dataset(objects: List[str]):
+    def etl_dataset():
         """
         ### Extract, Transform & Load (ETL) Excel into Parquet files
         Extracts data from the following Excel yearly-partitioned spreadsheets stored
-        on the 'raw_data' GCS bucket.
+        on the `bucket.raw_data.name` GCS bucket.
 
         Performs transformation to clean the data & exports them as parquet files
-        in then 'dataset' GCS bucket.
+        in then `bucket.dataset.name` GCS bucket.
         """
