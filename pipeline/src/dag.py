eaf1732f615f72b6cf42ba859959aabd3fe4a826
Zhu Zhanyan <program.nom@gmail.com>
2022-10-05 21:26:24 +0800

    refactor(pipeline): add transform_dataset() step to pipeline dag

--- pipeline/src/dag.py
+++ pipeline/src/dag.py
@@ -4,20 +4,15 @@
 # Airflow DAG
 #
 
-from os import path
-from tempfile import TemporaryDirectory
-from typing import Any, Dict, Optional, cast
+from typing import cast
 
-import pandas as pd
 from airflow.decorators import dag, task
-from airflow.models.dag import DAG
-from airflow.providers.google.cloud.hooks.gcs import GCSHook
 from pendulum import datetime
 from pendulum.datetime import DateTime
 from pendulum.tz import timezone
 
-from clean import clean_p6
-from prepare import prepare_extract
+from clean import clean_extract, clean_p6
+from transform import suffix_subject_level
 
 TIMEZONE = timezone("Asia/Singapore")
 config = {
@@ -65,7 +60,7 @@ def pipeline(
     The pipeline takes in as data source 2 kinds of Excel Spreadsheets stored
     in the `raw_bucket` GCS bucket, partitioned by year:
     - Sec 4 Cohort Sendout, stored as `raw_s4_prefix/<YEAR>.xlsx`
-    - P6 Screening Template, stored as `raw_p6_prefix/<YEAR>.xlsx`
+    - Optional P6 Screening Template, stored as `raw_p6_prefix/<YEAR>.xlsx`
 
     The pipeline assumes that all dates / times are expressed in the Asia/Singapore time zone.
 
@@ -74,56 +69,32 @@ def pipeline(
     `models_bucket` GCS bucket.
     """
 
-    @task(task_id="clean_data")
-    def clean_data(data_interval_start: Optional[DateTime] = None):
+    @task(
+        task_id="transform_dataset",
+    )
+    def transform_dataset(data_interval_start: DateTime = cast(DateTime, None)):
         """
-        ### Clean Data
-        Extracts data from the following Excel yearly-partitioned spreadsheets stored
-        on the `bucket.raw_data.name` GCS bucket.
+        Transform the Data source Excel Spreadsheets into Parquet Dataset.
+        Both data source & dataset are partitioned by cohort year.
+        """
+        import pandas as pd
+        from airflow.providers.google.cloud.hooks.gcs import GCSHook
 
-        Processes the data to clean it
-        & loads them as parquet files in into the `bucket.datasets.name` GCS bucket
-        as year-partitioned parquet files under the `bucket.datasets.clean_prefix`.
-        """
-        sg_begin = cast(DateTime, data_interval_start).astimezone(TIMEZONE)
-        raw_data, year = config["buckets"]["raw_data"], sg_begin.year
+        # load & Clean data from excel spreadsheet(s)
+        year = data_interval_start.year
+        df = clean_extract(
+            pd.read_excel(f"gs://{raw_bucket}/{raw_s4_prefix}/{year}.xlsx")
+        )
+        # merge in cleaned p6 data if it exists
         gcs = GCSHook()
+        if gcs.exists(raw_bucket, f"{raw_p6_prefix}/{year}.xlsx"):
+            p6_df = clean_p6(pd.read_excel(f"gs://{raw_bucket}/{raw_p6_prefix}"))
+            df = pd.merge(df, p6_df, how="left", on="Serial number")
 
-        # Download data as Excel Spreadsheets
-        with TemporaryDirectory(prefix=str(year)) as work_dir:
-            # download Sec 4 Cohort sendout spreadsheet
-            src_s4_path = f"{raw_data['sec4_prefix']}/{year}.xlsx"
-            dest_s4_path = path.join(work_dir, "s4.xlsx")
-            if not gcs.exists(raw_data["name"], src_s4_path):
-                raise FileNotFoundError(
-                    f"Expected S4 Cohort Excel Spreadsheet to exist: {src_s4_path}"
-                )
-            gcs.download(raw_data["name"], src_s4_path, dest_s4_path)
-            # download P6 screening spreadsheet if it exists
-            src_p6_path = f"{raw_data['p6_prefix']}/{year}.xlsx"
-            dest_p6_path = path.join(work_dir, "p6.xlsx")
-            if gcs.exists(raw_data["name"], src_p6_path):
-                gcs.download(raw_data["name"], src_p6_path, dest_p6_path)
+        # suffix subjects with level the subject was taken
+        df = suffix_subject_level(df, year)
 
-            # Extract & Transform data from spreadsheets to Parquet
-            df = prepare_extract(pd.read_excel(dest_s4_path), year)
-            # merge in p6 screening data if present
-            if path.exists(dest_p6_path):
-                # header=1: headers are stored in p6 data on the second row
-                p6_df = clean_p6(pd.read_excel(dest_p6_path, header=1))
-                df = pd.merge(df, p6_df, how="left", on="Serial number")
+        # write transfomred dataset as compressed parquet file
+        df.to_parquet(f"gs://{datasets_bucket}/dataset_{year}.pq")
 
-            # serial no. column no longer needed post join.
-            df = df.drop(columns=["Serial number"])
-            # write dataframe as parquet files
-            df.to_parquet(f"{year}.parquet")
-
-            # Upload data as parquet files
-            datasets = config["buckets"]["datasets"]
-            gcs.upload(
-                datasets["name"],
-                object_name=f"{datasets['clean_prefix']}/{year}.parquet",
-                filename=f"{year}.parquet",
-            )
-
-    clean_data()
+    transform_dataset()
