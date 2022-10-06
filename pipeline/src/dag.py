1ae7e7f96773513599b147df353a6ff30f21ba64
Zhu Zhanyan <program.nom@gmail.com>
2022-10-06 02:48:50 +0000

    fix(pipeline): gcp auth needed to pull files from GCS in transform_dataset() task

--- pipeline/src/dag.py
+++ pipeline/src/dag.py
@@ -4,24 +4,27 @@
 # Airflow DAG
 #
 
+import json
 from typing import cast
 
 from airflow.decorators import dag, task
+from airflow.models.connection import Connection
 from pendulum import datetime
 from pendulum.datetime import DateTime
 from pendulum.tz import timezone
+from pendulum.tz.timezone import Timezone
 
 from clean import clean_extract, clean_p6
 from transform import suffix_subject_level
 
-TIMEZONE = timezone("Asia/Singapore")
+TIMEZONE = "Asia/Singapore"
 
 
 @dag(
     dag_id="sortin-hat-pipeline",
     description="Sortin-hat ML Pipeline",
     # each dag run handles a year-sized data interval from start_date
-    start_date=datetime(2016, 1, 1, tz=TIMEZONE),
+    start_date=datetime(2016, 1, 1, tz=timezone(TIMEZONE)),
     schedule_interval="@yearly",
 )
 def pipeline(
@@ -30,6 +33,7 @@ def pipeline(
     raw_p6_prefix: str = "P6_Screening",
     datasets_bucket: str = "sss-sortin-hat-datasets",
     models_bucket: str = "sss-sortin-hat-models",
+    timezone_str: str = TIMEZONE,
 ):
     """
     # Sortin-hat ML Pipeline
@@ -38,6 +42,7 @@ def pipeline(
     ## Prerequisites
     ### Connections
     Expects a GCP connection to be configured with the id `google_cloud_default`
+    with extra for keyfile json set.
 
     ### Infrastructure
     GCS buckets `raw_bucket`, `datasets_bucket` & `models_bucket` should be created
@@ -56,35 +61,66 @@ def pipeline(
     `models_bucket` GCS bucket.
     """
 
+    # extract gcp service account json key path from airflow gcp connection
+    gcp_key_path = json.loads(
+        Connection.get_connection_from_secrets("google_cloud_default").get_extra()  # type: ignore
+    )["extra__google_cloud_platform__key_path"]
+
     @task(
         task_id="transform_dataset",
     )
-    def transform_dataset(data_interval_start: DateTime = cast(DateTime, None)):
+    def transform_dataset(
+        raw_bucket: str,
+        raw_s4_prefix: str,
+        raw_p6_prefix: str,
+        datasets_bucket: str,
+        timezone_str: str,
+        data_interval_start: DateTime = cast(DateTime, None),
+    ) -> str:
         """
         Transform the Data source Excel Spreadsheets into Parquet Dataset.
         Both data source & dataset are partitioned by cohort year.
+        Returns the path the Parquet Dataset in GCS.
         """
+        print("raw_bucket:", raw_bucket)
+        print("raw_s4_prefix:", raw_s4_prefix)
+        print("raw_p6_prefix:", raw_p6_prefix)
+        print("datasets_bucket:", datasets_bucket)
+        print("data_interval_start", data_interval_start)
         import pandas as pd
         from airflow.providers.google.cloud.hooks.gcs import GCSHook
 
+        # data interval's year in local time zone
+        year = data_interval_start.astimezone(timezone(timezone_str)).year
         # load & Clean data from excel spreadsheet(s)
-        year = data_interval_start.year
+        storage_options = {"token": gcp_key_path}
         df = clean_extract(
-            pd.read_excel(f"gs://{raw_bucket}/{raw_s4_prefix}/{year}.xlsx")
+            pd.read_excel(
+                f"gs://{raw_bucket}/{raw_s4_prefix}/{year}.xlsx",
+                storage_options=storage_options,
+            )
         )
         # merge in cleaned p6 data if it exists
         gcs = GCSHook()
         if gcs.exists(raw_bucket, f"{raw_p6_prefix}/{year}.xlsx"):
-            p6_df = clean_p6(pd.read_excel(f"gs://{raw_bucket}/{raw_p6_prefix}"))
+            p6_df = clean_p6(
+                pd.read_excel(
+                    f"gs://{raw_bucket}/{raw_p6_prefix}.xlsx",
+                    storage_options=storage_options,
+                )
+            )
             df = pd.merge(df, p6_df, how="left", on="Serial number")
-
         # suffix subjects with level the subject was taken
         df = suffix_subject_level(df, year)
-
         # write transformed dataset as compressed parquet file
-        df.to_parquet(f"gs://{datasets_bucket}/dataset_{year}.pq")
+        dataset_path = f"gs://{datasets_bucket}/dataset_{year}.pq"
+        df.to_parquet(dataset_path, storage_options=storage_options)
 
-    transform_dataset()
+        return dataset_path
+
+    transform_dataset(
+        raw_bucket, raw_s4_prefix, raw_p6_prefix, datasets_bucket, timezone_str
+    )
 
 
 dag = pipeline()
