88e77412f674e8e03aa4fd0f9b82f04244afc37e
Zhu Zhanyan <program.nom@gmail.com>
2022-10-09 21:50:58 +0000

    build(terraform): relax version constraint for terraform to any version above 1.2.6

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -21,7 +21,7 @@ locals {
 
 # Terraform & Provider config
 terraform {
-  required_version = "~>1.2.6"
+  required_version = ">= 1.2.6"
   required_providers {
     google = {
       source  = "hashicorp/google"
