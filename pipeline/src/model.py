41722b725a73b8753f27a3b9b62b9c950dccb184
Zhu Zhanyan <program.nom@gmail.com>
2022-10-11 06:28:55 +0000

    feat(pipeline): add linear regression model

--- pipeline/src/model.py
+++ pipeline/src/model.py
@@ -4,27 +4,17 @@
 # ML Models
 #
 
-
 from abc import ABC, abstractmethod
 from pathlib import Path
 
 import numpy as np
+from mlflow import sklearn
 from numpy.typing import NDArray
+from sklearn.linear_model import ElasticNet
 
 
 class Model(ABC):
     """Abstract ML Model interface implemented by all models."""
-
-    @classmethod
-    @abstractmethod
-    def load(cls, path: Path):
-        """Restore a Model from the file at the given path."""
-        pass
-
-    @abstractmethod
-    def save(self, path: Path):
-        """Restore a Model from the file at the given path."""
-        pass
 
     @abstractmethod
     def fit(self, features: NDArray[np.float_], labels: NDArray[np.float_]):
@@ -35,3 +25,35 @@ class Model(ABC):
     def predict(self, features: NDArray[np.float_]) -> NDArray[np.float_]:
         """Make a batch of predictions using the given input features."""
         pass
+
+    @abstractmethod
+    def save(self, path: Path):
+        """Store the Model at the given path in the MLFlow model's format."""
+        pass
+
+    @classmethod
+    @abstractmethod
+    def load(cls, path: Path):
+        """Restore a Model from the MLFlow model at the given path."""
+        pass
+
+
+class LinearRegression(Model):
+    """Linear Regression ML Model"""
+
+    def __init__(self) -> None:
+        self.model = ElasticNet()
+
+    def fit(self, features: NDArray[np.float_], labels: NDArray[np.float_]):
+        self.model.fit(features, labels)
+
+    def predict(self, features: NDArray[np.float_]) -> NDArray[np.float_]:
+        return self.model.predict(features)
+
+    def save(self, path: Path):
+        sklearn.save_model(self.model, path.as_posix())
+        self.model.get_params()
+
+    @classmethod
+    def load(cls, path: Path):
+        return cls(sklearn.load_model(path.as_posix()))
