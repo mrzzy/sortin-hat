5715a9a70a0b0fc0ca6017c931617fae98fff4fc
Zhu Zhanyan <program.nom@gmail.com>
2022-10-12 07:05:17 +0800

    wip: feat(pipeline): add hyperparameter tuning support to LinearRegression model

--- pipeline/src/model.py
+++ pipeline/src/model.py
@@ -6,15 +6,35 @@
 
 from abc import ABC, abstractmethod
 from pathlib import Path
+from typing import Dict
 
 import numpy as np
 from mlflow import sklearn
 from numpy.typing import NDArray
+from ray import tune
+from sklearn.base import BaseEstimator, RegressorMixin
 from sklearn.linear_model import ElasticNet
 
 
 class Model(ABC):
     """Abstract ML Model interface implemented by all models."""
+
+    @staticmethod
+    @abstractmethod
+    def param_space() -> Dict:
+        """Defining hyperparameters a search space for the model.
+
+        Returns:
+            Dictionary representing he hyperparameter search space in terms of
+            Ray Tune's Search Space API.
+        """
+        pass
+
+    @classmethod
+    @abstractmethod
+    def build(cls, params: Dict) -> None:
+        """Build the model with the given dictionary of hyperparameters."""
+        pass
 
     @abstractmethod
     def fit(self, features: NDArray[np.float_], labels: NDArray[np.float_]):
@@ -39,10 +59,21 @@ class Model(ABC):
 
 
 class LinearRegression(Model):
-    """Linear Regression ML Model"""
+    """Linear Regression model based on Sckit-learn's ElasticNet."""
 
-    def __init__(self) -> None:
-        self.model = ElasticNet()
+    def __init__(self, model):
+        self.model = model
+
+    @staticmethod
+    def param_space() -> Dict:
+        return  # TODO:
+
+    @classmethod
+    def build(self, params: Dict):
+        self.model = ElasticNet(
+            alpha=params["l2_regularize"],
+            l1_ratio=params["l1_regularize"],
+        )
 
     def fit(self, features: NDArray[np.float_], labels: NDArray[np.float_]):
         self.model.fit(features, labels)
